0f5c5d33a22791ef305f0acdd4e9470b84251677
Michal Wrona <michal.wrona.4@gmail.com>
2016-03-30 12:11:40 +0200

    VFS-1754 Create default configs

--- example_docker/run_luma.sh
+++ example_docker/run_luma.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 cd /root/luma
-
-# Use example configs
-cp example_config/config.cfg.example config.cfg
-cp example_config/generators_mapping.json.example generators_mapping.json
-cp generators/generators.cfg.example generators/generators.cfg
-
 # Init database and start LUMA server
 ./init_db.py
 ./main.py -gm generators_mapping.json
