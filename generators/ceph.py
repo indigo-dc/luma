aabf4fecfa9cb126a84dd28c06d1ec40c8e650e5
Michal Wrona <michal.wrona.4@gmail.com>
2016-03-31 11:02:51 +0200

    VFS-1754 Restore ceph generator

--- generators/ceph.py
+++ generators/ceph.py
@@ -1,12 +1,16 @@
 import os
+import rados
+import json
 import ConfigParser
 
 config = ConfigParser.RawConfigParser()
 config.read(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), 'generators.cfg'))
 
+POOL_NAME = config.get('ceph', 'pool_name')
 USER = config.get('ceph', 'user')
 KEY = config.get('ceph', 'key')
+MON_HOST = config.get('ceph', 'mon_host')
 
 
 def create_user_credentials(global_id, storage_type, storage_id, source_ips,
@@ -18,5 +22,19 @@ def create_user_credentials(global_id, storage_type, storage_id, source_ips,
         "user_key": "KEY"
     }
     """
-    return {"user_name": USER, "user_key": KEY}
+    if global_id == "0":
+        return {"user_name": USER, "user_key": KEY}
+    cluster = rados.Rados(conf=dict())
+    cluster.conf_set("key", KEY)
+    cluster.conf_set("mon host", MON_HOST)
+    cluster.connect()
+    user_name = "client.{0}".format(global_id)
 
+    status, response, reason = cluster.mon_command(json.dumps(
+        {"prefix": "auth get-or-create", "entity": user_name,
+         "caps": ["mon", "allow r", "osd",
+                  "allow rw pool={}".format(POOL_NAME)]}), "")
+    if status != 0:
+        raise RuntimeError(reason)
+
+    return {"user_name": user_name, "user_key": response.split()[-1]}
